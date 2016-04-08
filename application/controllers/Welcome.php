0a8aa0c444defb182e88628cedae2d6c09b4e74b
Hastwell <turner8193@gmail.com>
2016-04-07 17:43:02 -0700

    Break out singlepage Welcome to include header+footer

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -6,12 +6,13 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		// Dict to pass to view class
-		$data['title'] = "My Unique Title Tag";
-		$data['page_id'] = "My Clever Page Id";
+		$data['title'] = "My Unique Title Tag, now inside Header!";
+		$data['page_id'] = "My Clever Page Id, now inside Header!";
 		
 		$data['guts'] = '<p>I am the creamy filling that Bill is teasing you with</p>';
 				
-		// Pass data to view class via additional param
+		$this->load->view('templates/header', $data);
 		$this->load->view('welcome_message', $data);
+		$this->load->view('templates/footer', $data);
 	}
 }
